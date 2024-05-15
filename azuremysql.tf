1a1ab8022b655f061f0d65a1f30205d85711d467
salomoncloud <158000174+salomoncloud@users.noreply.github.com>
2024-05-15 19:26:36 -0400

    Update azuremysql.tf

--- azuremysql.tf
+++ azuremysql.tf
@@ -1,20 +1,18 @@
-resource "azurerm_mysql_server" "salomon_db_mysql" {
-  name                = "salomondbmysql"
-  location            = azurerm_resource_group.salomon_db.location
-  resource_group_name = azurerm_resource_group.salomon_db.name
-
+resource "azurerm_mssql_server" "salomon_db_mysql" {
+  name                         = "salomondbmysql"
+  resource_group_name          = azurerm_resource_group.salomon_db.name
+  location                     = azurerm_resource_group.salomon_db.location
+  version                      = var.version_for_the_mysql_db
   administrator_login          = var.administrator_login
   administrator_login_password = var.administrator_login_password
+  minimum_tls_version          = "1.2"
 
-  sku_name   = var.sku
-  storage_mb = var.storage_mb
-  version    = var.version_for_the_mysql_db
+  azuread_administrator {
+    login_username = "AzureAD Admin"
+    object_id      = "00000000-0000-0000-0000-000000000000"
+  }
 
-  auto_grow_enabled                 = true
-  backup_retention_days             = 7
-  geo_redundant_backup_enabled      = false
-  infrastructure_encryption_enabled = false
-  public_network_access_enabled     = true
-  ssl_enforcement_enabled           = true
-  ssl_minimal_tls_version_enforced  = "TLS1_2"
+  tags = {
+    environment = "production"
+  }
 }
