119c011cac04cab22de12edf117cbdee6705aa8b
salomoncloud <158000174+salomoncloud@users.noreply.github.com>
2024-05-15 19:47:25 -0400

    Update local.tf

--- local.tf
+++ local.tf
@@ -2,7 +2,7 @@ locals{
   mysql_app=[for f in fileset("${path.module}/config", "[^_]*.yaml") : yamldecode(file("${path.module}/configs/${f}"))]
   mysql_app_list = flatten([
     for app in local.mysql_app : [
-      for mysqlapps in try(app.listofmysqlapp, []) :{
+      for mysqlapps in try(app.listofmysqlapps, []) :{
         name=mysqlapps.name    
       }
     ]
